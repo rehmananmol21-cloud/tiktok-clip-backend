88796895ced3918a4643e3efe97e0577c374249b
rehmananmol21-cloud <rehmananmol21@gmail.com>
2025-12-30 15:53:00 +0500

    Update server.js
    fix railway port

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Railway PORT (VERY IMPORTANT)
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Backend live hai 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running on port", PORT);
 });
