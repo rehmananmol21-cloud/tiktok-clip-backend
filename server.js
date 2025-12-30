17a80bd9edc8d5ebc03960f96c29da4d2aab9211
rehmananmol21-cloud <rehmananmol21@gmail.com>
2025-12-30 15:43:57 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend chal raha hai 🚀");
+  res.send("Backend live hai 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
